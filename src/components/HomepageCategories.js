bb408f623b854432e3071742e227d19f0f461d9f
vilsku <aspegren.vilja@gmail.com>
2021-10-15 00:38:34 +0300

    testi

--- src/components/HomepageCategories.js
+++ src/components/HomepageCategories.js
@@ -18,45 +18,144 @@ const data =  [
 function HomepageCategories() {
 
   
-    return(
-        <div>
-        
-        <div style={{borderWidth: 1, borderColor: "grey", borderStyle: "solid", margin: 20, borderRadius: 10, display: "flex", flexDirection: "row", backgroundColor: 'lightgray', boxShadow: '0px 0px 10px gray', width: '30%', height: 35, marginLeft: '60%', marginRight: '50%', marginRight: 10, marginBottom: 20}}>
-          <div style={{flex: 4}}>
-          <p style={{marginLeft: 20, marginTop: 5}}>Search...</p>
-      </div>
+    return (
+      <div style={{ width: "100%" }}>
+        <div class='header' style={{width: "80%", margin: "10px auto", display: "flex", flexdirection: "row", justifyContent: "space-between"}}>
+          <h1>BSafe</h1>
+          <div
+            style={{
+              borderWidth: 1,
+              borderColor: "grey",
+              borderStyle: "solid",
+              margin: "20px 0px 0px 0px",
+              borderRadius: 10,
+              display: "flex",
+              flexDirection: "row",
+              backgroundColor: "lightgray",
+              boxShadow: "0px 0px 10px gray",
+              width: "40%",
+              height: 35,
+              marginRight: "50%",
+              marginRight: 10,
+              marginBottom: 20,
+            }}
+          >
+            <div style={{ flex: 4 }}>
+              <p style={{ marginLeft: 20, marginTop: 5 }}>Search...</p>
+            </div>
+            <div style={{ flex: 1 }}>
+              <p style={{ marginTop: "20%", fontSize: 20 }}>
+                <FaSearch />
+              </p>
+            </div>
+          </div>
         </div>
 
-      <div style={{borderWidth: 1, borderColor: "grey", borderStyle: "solid", margin: 5, borderRadius: 10, display: "flex", flexDirection: "row", backgroundColor: '#d8b9ff', boxShadow: '0px 0px 10px gray', width: '30%', float: 'left', height: 40}}>
-          <div style={{flex: 3}}>
-          <h4 style={{marginLeft: 0}}>Stages</h4>
-      </div>
+        <div
+          style={{
+            borderWidth: 1,
+            borderColor: "grey",
+            borderStyle: "solid",
+            margin: 5,
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "#d8b9ff",
+            boxShadow: "0px 0px 10px gray",
+            width: "30%",
+            float: "left",
+            height: 40,
+          }}
+        >
+          <div style={{ flex: 3 }}>
+            <h4 style={{ marginLeft: 0 }}>Stages</h4>
+          </div>
         </div>
 
-        <div style={{borderWidth: 1, borderColor: "grey", borderStyle: "solid", margin: 5, borderRadius: 10, display: "flex", flexDirection: "row", backgroundColor: '#d8b9ff', boxShadow: '0px 0px 10px gray', width: '30%', float: 'left' , height: 40}}>
-          <div style={{flex: 3}}>
-          <h4 style={{marginLeft: 0}}>Lights</h4>
-      </div>
+        <div
+          style={{
+            borderWidth: 1,
+            borderColor: "grey",
+            borderStyle: "solid",
+            margin: 5,
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "#d8b9ff",
+            boxShadow: "0px 0px 10px gray",
+            width: "30%",
+            float: "left",
+            height: 40,
+          }}
+        >
+          <div style={{ flex: 3 }}>
+            <h4 style={{ marginLeft: 0 }}>Lights</h4>
+          </div>
         </div>
 
-        <div style={{borderWidth: 1, borderColor: "gray", borderStyle: "solid", margin: 5, borderRadius: 10, display: "flex", flexDirection: "row", backgroundColor: '#d8b9ff', boxShadow: '0px 0px 10px gray', width: '30%', float: 'left' , height: 40}}>
-          <div style={{flex: 3}}>
-          <h4 style={{marginLeft: 0}}>Events</h4>
-      </div>
+        <div
+          style={{
+            borderWidth: 1,
+            borderColor: "gray",
+            borderStyle: "solid",
+            margin: 5,
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "#d8b9ff",
+            boxShadow: "0px 0px 10px gray",
+            width: "30%",
+            float: "left",
+            height: 40,
+          }}
+        >
+          <div style={{ flex: 3 }}>
+            <h4 style={{ marginLeft: 0 }}>Events</h4>
+          </div>
         </div>
 
-        <div style={{borderWidth: 1, borderColor: "gray", borderStyle: "solid", margin: 5, borderRadius: 10, display: "flex", flexDirection: "row", backgroundColor: '#d8b9ff', boxShadow: '0px 0px 10px gray', width: '30%', float: 'left' , height: 40}}>
-          <div style={{flex: 3}}>
-          <h4 style={{marginLeft: 0}}>Ergonomy</h4>
-      </div>
+        <div
+          style={{
+            borderWidth: 1,
+            borderColor: "gray",
+            borderStyle: "solid",
+            margin: 5,
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "#d8b9ff",
+            boxShadow: "0px 0px 10px gray",
+            width: "30%",
+            float: "left",
+            height: 40,
+          }}
+        >
+          <div style={{ flex: 3 }}>
+            <h4 style={{ marginLeft: 0 }}>Ergonomy</h4>
+          </div>
         </div>
 
-        <div style={{borderWidth: 1, borderColor: "gray", borderStyle: "solid", margin: 5, borderRadius: 10, display: "flex", flexDirection: "row", backgroundColor: '#d8b9ff', boxShadow: '0px 0px 10px gray', width: '30%', float: 'left' , height: 40}}>
-          <div style={{flex: 3}}>
-          <h4 style={{marginLeft: 0}}>Electricity</h4>
+        <div
+          style={{
+            borderWidth: 1,
+            borderColor: "gray",
+            borderStyle: "solid",
+            margin: 5,
+            borderRadius: 10,
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "#d8b9ff",
+            boxShadow: "0px 0px 10px gray",
+            width: "30%",
+            float: "left",
+            height: 40,
+          }}
+        >
+          <div style={{ flex: 3 }}>
+            <h4 style={{ marginLeft: 0 }}>Electricity</h4>
+          </div>
+        </div>
       </div>
-        </div>
-        </div>
     );
   }
   export default HomepageCategories;
